5359b74de20957ab7333117bbdc27c954088a90e
Kristina Cerniauskaite <k.cerniauskaite@gmail.com>
2021-06-14 09:49:47 +0200

    added cart notification

---
+++ truyum-ui/js/script.js
@@ -0,0 +1,6 @@
+const div_results = document.getElementById("result");
+
+function alertMessage() {
+    div_results.innerHTML = "<p> Item removed from Cart successfully </p>";
+    return false;
+}
